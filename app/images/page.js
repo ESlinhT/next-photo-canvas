461959118271772af25b6af99d94df0e816102c6
Linh Hoang <lh@buildmidwestern.com>
2024-10-04 15:47:04 -0500

    added blue background

--- app/images/page.js
+++ app/images/page.js
@@ -9,6 +9,7 @@ import {signOut} from "@/app/lib/appwrite";
 import {useRouter} from "next/navigation";
 import Link from "next/link";
 import React from "react";
+import BlueBackground from "@/app/components/BlueBackground";
 
 
 const userNavigation = [{name: 'Your profile', href: '#'}, {name: 'Sign out', href: '#'},]
@@ -51,32 +52,35 @@ export default function Images() {
 
     return (
         <>
-            {!user ? <div className="h-[100vh] flex flex-col justify-center items-center">
-                <h2 className="text-3xl font-bold">Please login to use this feature</h2>
-                <div className="mt-10 ">
-                    <Link href={'/login'}
-                          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:indigo focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                        Sign in here
-                    </Link>
-                </div>
-            </div> : <>
-                <ImageSidebar/>
-                <div className="lg:pl-72 bg-white">
-                    <div
-                        className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+            {!user ?
+                <BlueBackground>
+                    <div className="h-[100vh] flex flex-col justify-center items-center">
+                        <h2 className="text-3xl font-bold text-gray-200">Please login to use this feature</h2>
+                        <div className="mt-10 ">
+                            <Link href={'/login'}
+                                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:indigo focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                Sign in here
+                            </Link>
+                        </div>
+                    </div>
+                </BlueBackground> : <>
+                    <ImageSidebar/>
+                    <div className="lg:pl-72 bg-white">
+                        <div
+                            className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
 
-                        <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6 justify-end">
-                            <div className="flex items-center gap-x-4 lg:gap-x-6">
+                            <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6 justify-end">
+                                <div className="flex items-center gap-x-4 lg:gap-x-6">
 
-                                <Menu as="div" className="relative">
-                                    <MenuButton className="-m-1.5 flex items-center p-1.5">
-                                        <span className="sr-only">Open user menu</span>
-                                        <img
-                                            alt=""
-                                            src={user?.avatar}
-                                            className="h-8 w-8 rounded-full bg-gray-50"
-                                        />
-                                        <span className="hidden lg:flex lg:items-center">
+                                    <Menu as="div" className="relative">
+                                        <MenuButton className="-m-1.5 flex items-center p-1.5">
+                                            <span className="sr-only">Open user menu</span>
+                                            <img
+                                                alt=""
+                                                src={user?.avatar}
+                                                className="h-8 w-8 rounded-full bg-gray-50"
+                                            />
+                                            <span className="hidden lg:flex lg:items-center">
                                                   <span aria-hidden="true"
                                                         className="ml-4 text-sm font-semibold leading-6 text-gray-900 capitalize">
                                                     {user?.username}
@@ -84,25 +88,25 @@ export default function Images() {
                                                   <ChevronDownIcon aria-hidden="true"
                                                                    className="ml-2 h-5 w-5 text-gray-400"/>
                                                 </span>
-                                    </MenuButton>
-                                    <MenuItems
-                                        transition
-                                        className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                                    >
-                                        {userNavigation.map((item) => handleItemClick(item))}
-                                    </MenuItems>
-                                </Menu>
+                                        </MenuButton>
+                                        <MenuItems
+                                            transition
+                                            className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                                        >
+                                            {userNavigation.map((item) => handleItemClick(item))}
+                                        </MenuItems>
+                                    </Menu>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <main className="py-10">
-                        <div className="flex m-5 justify-center">
-                            <PhotoCanvas/>
-                        </div>
-                    </main>
-                </div>
-            </>}
+                        <main className="py-10">
+                            <div className="flex m-5 justify-center">
+                                <PhotoCanvas/>
+                            </div>
+                        </main>
+                    </div>
+                </>}
         </>
     )
 }
--- app/login/page.js
+++ app/login/page.js
@@ -4,13 +4,14 @@ import React, {useState} from 'react';
 import Link from "next/link";
 import {signIn} from "@/app/lib/appwrite";
 import {useRouter} from "next/navigation";
-import { useGlobalContext } from "@/app/context/GlobalProvider";
+import {useGlobalContext} from "@/app/context/GlobalProvider";
+import BlueBackground from "@/app/components/BlueBackground";
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const router = useRouter();
-    const { init } = useGlobalContext();
+    const {init} = useGlobalContext();
 
     const handleSubmit = async () => {
         try {
@@ -25,71 +26,77 @@ const Login = () => {
     };
 
     return (
-        <div className="flex h-[100vh] flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <h2 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-800">Next Photo
-                    Canvas</h2>
-            </div>
+        <BlueBackground>
+            <div className="flex h-[100vh] flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-200">Next
+                        Photo
+                        Canvas</h2>
+                </div>
 
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6">
-                    <div>
-                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                            Email
-                        </label>
-                        <div className="mt-2">
-                            <input
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                id="email"
-                                name="email"
-                                type="email"
-                                required
-                                autoComplete="email"
-                                className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className="flex items-center justify-between">
-                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                Password
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6">
+                        <div>
+                            <label htmlFor="email"
+                                   className="block text-sm font-medium leading-6 text-gray-200">
+                                Email
                             </label>
+                            <div className="mt-2">
+                                <input
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    required
+                                    autoComplete="email"
+                                    className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
                         </div>
-                        <div className="mt-2">
-                            <input
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                id="password"
-                                name="password"
-                                type="password"
-                                required
-                                autoComplete="current-password"
-                                className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password"
+                                       className="block text-sm font-medium leading-6 text-gray-200">
+                                    Password
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    required
+                                    autoComplete="current-password"
+                                    className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
                         </div>
-                    </div>
 
-                    <div>
-                        <button
-                            type="button"
-                            onClick={handleSubmit}
-                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Sign in
-                        </button>
-                    </div>
-                </form>
+                        <div>
+                            <button
+                                type="button"
+                                onClick={handleSubmit}
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
 
-                <p className="mt-10 text-center text-sm text-gray-500">
-                    Not a member?{' '}
-                    <Link href={'/signup'} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                        Sign up here
-                    </Link>
-                </p>
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        Not a member?{' '}
+                        <Link href={'/signup'}
+                              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                            Sign up here
+                        </Link>
+                    </p>
+                </div>
             </div>
-        </div>
+        </BlueBackground>
     );
 };
 
