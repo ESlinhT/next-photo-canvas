d73dfb3620d62ede7fadb148a97e5470c58a0690
Linh Hoang <lh@buildmidwestern.com>
2024-11-05 08:31:16 -0600

    another fix

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import BlueBackgroundLayout from "@/app/layouts/BlueBackgroundLayout";
 export default function Home() {
     return (
         <BlueBackgroundLayout>
-            <div className="mx-auto max-w-7xl min-h-[100vh] px-6 lg:flex lg:flex-row pt-2 lg:pt-0 lg:px-8 sm:flex sm:flex-col justify-center items-center">
+            <div className="mx-auto max-w-7xl min-h-[100vh] px-6 lg:flex lg:flex-row pt-4 lg:pt-0 lg:px-8 sm:flex sm:flex-col justify-center items-center">
                 <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
                     <h1 className="uppercase text-4xl font-bold tracking-tight text-indigo-500">Next Photo Canvas</h1>
                     <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl capitalize">
@@ -16,7 +16,7 @@ export default function Home() {
                         This is a project that attempts to clone the functionality of creating photos and photobooks on
                         sites such as MPix, using React Canvas.
                     </p>
-                    <div className="mt-10 flex items-center gap-x-6">
+                    <div className="mt-4 lg:mt-10 flex items-center gap-x-6">
                         <Link
                             className="bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
                             href="/photos" >Photos</Link>
@@ -27,14 +27,14 @@ export default function Home() {
                     </div>
                 </div>
                 <div
-                    className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+                    className="mx-auto mt-4 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
                     <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
                         <Image
                             alt="App screenshot"
                             src={randomBg.src}
                             width={540}
                             height={540}
-                            className="w-[76rem] bg-white/5 shadow-2xl ring-1 ring-white/10 max-w-sm lg:max-w-[540px]"
+                            className="w-[76rem] bg-white/5 shadow-2xl ring-1 ring-white/10 max-w-xs lg:max-w-[540px]"
                         />
                     </div>
                 </div>
